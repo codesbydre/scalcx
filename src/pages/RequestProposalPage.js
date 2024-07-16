2d4f5bd883134f3bbe51406bf830dd20d2e6d10f
codesbydre <gabaldonandrea@gmail.com>
2024-07-16 13:51:38 -0700

    add more fields to proposal request form

--- src/pages/RequestProposalPage.js
+++ src/pages/RequestProposalPage.js
@@ -15,10 +15,21 @@ const RequestProposalPage = () => {
           <input type="text" className="form-control" required />
         </div>
         <div className="form-group">
+          <label>Telephone (required)</label>
+          <input type="tel" className="form-control" required />
+        </div>
+        <div className="form-group">
           <label>Email (required)</label>
           <input type="email" className="form-control" required />
         </div>
-        {/* Add more inputs */}
+        <div className="form-group">
+          <label>Project Address</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>Building SQ FT</label>
+          <input type="number" className="form-control" />
+        </div>
         <button type="submit" className="btn btn-primary">
           Request
         </button>
