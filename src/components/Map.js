6b364ce5886977b3d67dbad787fe7c6270c86236
codesbydre <gabaldonandrea@gmail.com>
2024-07-16 13:48:44 -0700

    add contact info, leaflet library

---
+++ src/components/Map.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+const position = [33.6708, -117.75966]; // Latitude and Longitude
+
+const markerIcon = new L.Icon({
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+const Map = () => {
+  return (
+    <MapContainer
+      center={position}
+      zoom={13}
+      style={{ height: "500px", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position} icon={markerIcon}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
--- src/pages/ContactInfoPage.js
+++ src/pages/ContactInfoPage.js
@@ -1,10 +1,21 @@
 import React from "react";
+import Map from "../components/Map";
 
 function ContactInfoPage() {
   return (
     <div className="container">
-      <h1>Contact Info:</h1>
-      <div>add address, map, contact, etc</div>
+      <h1>Contact Us</h1>
+      <img
+        className="brand-logo"
+        src={`${process.env.PUBLIC_URL}/scalcx-heading.jpg`}
+        alt="SCALX Logo"
+        style={{ height: "220px" }}
+      />
+      <div>102 Discovery, Irvine CA 92618</div>
+      <div>949-880-2523</div>
+      <div style={{ marginTop: "20px" }}>
+        <Map /> {/* Display the map here */}
+      </div>
     </div>
   );
 }
